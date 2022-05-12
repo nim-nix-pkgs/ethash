6ca72e914da8310c7f5943c489c1c2e496920f13
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:23 -0300

    chore: re index ethash

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."ethash-master".dir   = "master";
+  inputs."ethash-master".owner = "nim-nix-pkgs";
+  inputs."ethash-master".ref   = "master";
+  inputs."ethash-master".repo  = "ethash";
+  inputs."ethash-master".type  = "github";
+  inputs."ethash-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ethash-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
